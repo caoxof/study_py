041c6e15caece3f559b7891274f018eed4b8f035
caoxof <caoxof@users.noreply.github.com>
2020-12-22 23:46:58 +0800

    设置congfig，style，生成图卡死

--- python_repos.py
+++ python_repos.py
@@ -1,3 +1,4 @@
+from pygal import style
 import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS,LightenStyle as LS
@@ -31,15 +32,44 @@ for repo_dict in repo_dicts:
     print('Description:',repo_dict['description'])
     print('#'*50)
 """
-names, stars = [], []
+#names, stars = [], []
+names,plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    #stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description']),          ##无内容，转为str='None'
+        'xlink': repo_dict['html_url'],
+        }
+#    print(type(repo_dict['description']))
+#    if repo_dict['description'] == None :
+#        print(repo_dict['name'])
+
+    repo_dicts.append(repo_dict)
+
+
 #可视化
-my_style = LS('#333366',base_style=LCS)
-chart = pygal.Bar(style=my_style,x_label_rotation=45,show_legend=False)
+#my_style = LS('#333366',base_style=LCS)
+
+##图标样式配置
+my_config = pygal.Config()               ##Config()-class实例
+my_config.x_label_rotation = 45          ##x标签旋转45°
+my_config.show_legend = False            ##是否显示图例
+my_config.truncate_label = 15            ##截断标签字符数
+my_config.show_y_guides = False          ##是否显示y轴参考线
+my_config.width = 1000
+######pygal 2.0.0变动Move font_size config to style.
+my_config.style = LS('#333366',base_style=LCS)
+my_config.style.title_font_size = 24           ##图标标题字号
+my_config.style.label_font_size = 14           ##副标签字号
+my_config.style.major_label_font_size = 18     ##主标签字号
+###########################################################
+#chart = pygal.Bar(style=my_style,x_label_rotation=45,show_legend=False)
+chart = pygal.Bar(my_config)
 chart.title = 'Most-Starred Python Projects on Github'
 chart.x_labels = names
-chart.add('',stars)
-chart.render_to_file('python_repos.svg')
+#chart.add('',stars)
+chart.add('',plot_dicts)
+chart.render_to_file('python_repos1.svg')
 
